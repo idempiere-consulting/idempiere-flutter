bcc2af7123b903d470299bd795290e3d40639204
MatthewLong13 <matteolongo71@gmail.com>
2021-12-30 18:14:45 +0100

    Language it-IT localstring

--- idempiere_app/lib/localestrings.dart
+++ idempiere_app/lib/localestrings.dart
@@ -5,17 +5,54 @@ class LocaleString extends Translations {
   Map<String, Map<String, String>> get keys => {
         //ENGLISH LANGUAGE
         'en_US': {
-          'hello': 'Hello World',
-          'changelang': 'Language   ',
+          'changelang': 'Language',
           'Calendar': 'Calendar',
           'Dashborad': 'Dashboard'
         },
         //HINDI LANGUAGE
         'it_IT': {
-          'hello': 'Ciao mondo',
           'changelang': 'Lingua   ',
           'Calendar': 'Calendario',
-          'Dashboard': 'Cruscotto'
+          'Dashboard': 'Cruscotto',
+          'CRM': 'CRM',
+          'Lead': 'Lead',
+          'Opportunity': 'Opportunità',
+          'ContactBP': 'Contatto',
+          'CustomerBP': 'Cliente',
+          'Task&Appuntamenti': 'Task',
+          'SalesOrder': 'Offerta',
+          'ProductList': 'Listino Prodotto',
+          'Invoice': 'Fattura',
+          'Payment': 'Incasso',
+          'Commission': 'Provvigione',
+          'Shipment': 'Documento di Trasporto',
+          'Ticket': 'Ticket',
+          'TicketTicket': 'Ticket',
+          'TicketCustomerTicket': 'Ticket Cliente',
+          'TicketTask': 'Task',
+          'TicketResourceAssignment': 'Ore',
+          'Maintenance': 'Intervento',
+          'MaintenanceCalendar': 'Calendario',
+          'MaintenanceMptask': 'Task',
+          'MaintenanceMpanomaly': 'Anomalia',
+          'MaintenanceMpwarehouse': 'Mag. Furgone',
+          'MaintenanceMppicking': 'Prelienvo Magazzino',
+          'MaintenanceInternaluseinventory': 'Movimento Magazzino',
+          'MaintenanceMpimportitem': 'Carico scheda Tecnica',
+          'PortalMp': 'Portale Cliente',
+          'PortalMpInvoicepo': 'Fatture di Acquisto',
+          'PortalMpPortaloffer': 'Offerte di Vendita',
+          'Purchase': 'Acquisti',
+          'PurchaseLead': 'Lead Fornitore',
+          'PurchaseProductwarehouseprice': 'Prodotto',
+          'Supplychain': 'Logistica',
+          'SupplychainProductwarehouse': 'Prodotto',
+          'SupplychainInventory': 'Inventario',
+          'SupplychainMaterialreceipt': 'Entrata Merce',
+          'VehicleEquipment': 'Mezzi e Attrezzature',
+          'VehicleEquipmentVehicle': 'Mezzi',
+          'VehicleEquipmentEquipment': 'Attrezzatura',
+          'DashboardAssetresource': 'Risorse Aziendali'
         },
       };
 }
