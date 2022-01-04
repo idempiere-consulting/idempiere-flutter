90576472b9ea49a2665c65351a283ee4fa2db08e
MatthewLong13 <matteolongo71@gmail.com>
2022-01-04 11:33:40 +0100

    initialSelected Page Maintenance

--- idempiere_app/lib/Screens/app/features/Maintenance_Mppicking/views/components/sidebar.dart
+++ idempiere_app/lib/Screens/app/features/Maintenance_Mppicking/views/components/sidebar.dart
@@ -24,7 +24,7 @@ class _Sidebar extends StatelessWidget {
             ),
             const Divider(thickness: 1),
             SelectionButton(
-              initialSelected: 0,
+              initialSelected: 5,
               data: [
                 SelectionButtonData(
                   activeIcon: EvaIcons.arrowBack,
