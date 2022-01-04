cddf5cd0a45bae971f4bfeb4a89a7bdbe08f8355
MatthewLong13 <matteolongo71@gmail.com>
2022-01-04 10:19:31 +0100

    Language it-IT Maintenance

--- idempiere_app/lib/Screens/app/features/Maintenance_Mppicking/views/components/sidebar.dart
+++ idempiere_app/lib/Screens/app/features/Maintenance_Mppicking/views/components/sidebar.dart
@@ -29,52 +29,76 @@ class _Sidebar extends StatelessWidget {
                 SelectionButtonData(
                   activeIcon: EvaIcons.arrowBack,
                   icon: EvaIcons.arrowBackOutline,
-                  label: "Dashboard",
+                  label: "Dashboard".tr,
                 ),
                 SelectionButtonData(
                   activeIcon: Icons.person,
                   icon: EvaIcons.personOutline,
-                  label: "Maintenance",
+                  label: "MaintenanceCalendar".tr,
                 ),
                 SelectionButtonData(
                   activeIcon: Icons.person,
                   icon: EvaIcons.personOutline,
-                  label: "MaintenanceCalendar",
+                  label: "MaintenanceMptask".tr,
                 ),
                 SelectionButtonData(
                   activeIcon: Icons.person,
                   icon: EvaIcons.personOutline,
-                  label: "MaintenanceMptask",
+                  label: "MaintenanceMpanomaly".tr,
                 ),
                 SelectionButtonData(
                   activeIcon: Icons.person,
                   icon: EvaIcons.personOutline,
-                  label: "MaintenanceMpanomaly",
+                  label: "MaintenanceMpwarehouse".tr,
                 ),
                 SelectionButtonData(
                   activeIcon: Icons.person,
                   icon: EvaIcons.personOutline,
-                  label: "MaintenanceMpwarehouse",
+                  label: "MaintenanceMppicking".tr,
                 ),
                 SelectionButtonData(
                   activeIcon: Icons.person,
                   icon: EvaIcons.personOutline,
-                  label: "MaintenanceMppicking",
+                  label: "MaintenanceInternaluseinventory".tr,
                 ),
                 SelectionButtonData(
                   activeIcon: Icons.person,
                   icon: EvaIcons.personOutline,
-                  label: "MaintenanceInternaluseinventory",
-                ),
-                SelectionButtonData(
-                  activeIcon: Icons.person,
-                  icon: EvaIcons.personOutline,
-                  label: "MaintenanceMpimportitem",
+                  label: "MaintenanceMpimportitem".tr,
                 ),
               ],
               onSelected: (index, value) {
                 //log("index : $index | label : ${value.label}");
-                Get.toNamed('/${value.label}');
+                //Get.toNamed('/${value.label}');
+                switch (index) {
+                  case 0:
+                    Get.offNamed('/Dashboard');
+                    break;
+
+                  case 1:
+                    Get.offNamed('/MaintenanceCalendar');
+                    break;
+                  case 2:
+                    Get.offNamed('/MaintenanceMptask');
+                    break;
+                  case 3:
+                    Get.offNamed('/MaintenanceMpanomaly');
+                    break;
+                  case 4:
+                    Get.offNamed('/MaintenanceMpwarehouse');
+                    break;
+                  case 5:
+                    Get.offNamed('/MaintenanceMppicking');
+                    break;
+                  case 6:
+                    Get.offNamed('/MaintenanceInternaluseinventory');
+                    break;
+                  case 7:
+                    Get.offNamed('/MaintenanceMpimportitem');
+                    break;
+
+                  default:
+                }
               },
             ),
             const Divider(thickness: 1),
