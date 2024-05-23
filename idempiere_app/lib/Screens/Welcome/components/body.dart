7fee60588472cd75a106d94f566258c6ffd6d398
Vincenzo <vincenzo.distaso98@gmail.com>
2024-05-23 12:49:18 +0200

    gima

--- idempiere_app/lib/Screens/Welcome/components/body.dart
+++ idempiere_app/lib/Screens/Welcome/components/body.dart
@@ -28,7 +28,7 @@ class Body extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              "iDempiere Consulting",
+              "GIMA SPORT",
               style: TextStyle(fontWeight: FontWeight.bold),
             ),
             SizedBox(height: size.height * 0.05),
